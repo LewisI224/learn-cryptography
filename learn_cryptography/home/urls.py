b85ee5ca08cc6af229fcaa01658f16d686fd4ae7
Lewis Inches <li10@hw.ac.uk>
2022-01-23 19:40:09 +0000

    implemented templating and css

--- learn_cryptography/home/urls.py
+++ learn_cryptography/home/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('learning/', views.learning, name='learning'),
     path('about/', views.about, name='public-about'),
     path('', views.home, name='public-home'),
     
--- learn_cryptography/home/views.py
+++ learn_cryptography/home/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+modules = [
+    {
+        'title': 'Scytale',
+        'difficulty': 'Beginner',
+        'time': '5',
+        'description': 'Scytale - a wooden rod which parchment would be wound around',
+    },
+    {
+        'title': 'Caesar Cipher',
+        'difficulty': 'Beginner',
+        'time': '10',
+        'description': 'A substitution cipher used since ancient times',
+    }
+]
 def home(request):
     return render(request, 'home/home.html')
 
 def about(request):
-    return render(request, 'home/about.html')
+    return render(request, 'home/about.html', {'title':'About'})
+
+def learning(request):
+    context = {
+        'modules' : modules,
+        'title' : 'learning'
+    }
+    return render(request, 'home/learning.html', context)
