1e8f9a1bd5295b7142526117f5e1285aa91cf65d
Lewis Inches <li10@hw.ac.uk>
2022-02-04 23:04:20 +0000

    allow user to update profile and auto resize profile image

--- learn_cryptography/home/views.py
+++ learn_cryptography/home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import LearningModules
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'home/home.html')
@@ -7,6 +8,7 @@ def home(request):
 def about(request):
     return render(request, 'home/about.html', {'title':'About'})
 
+@login_required
 def learning(request):
     context = {
         'modules' : LearningModules.objects.all(),
