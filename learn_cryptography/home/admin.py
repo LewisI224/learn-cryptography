eeff3a6b197d8a714eccf071bf1bf00bf2cb7f04
Lewis Inches <li10@hw.ac.uk>
2022-02-06 00:20:04 +0000

    added tracking of current module, changed path matching for learning pages

--- learn_cryptography/home/admin.py
+++ learn_cryptography/home/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import LearningModules
+from .models import LearningModules, Tag
 
 admin.site.register(LearningModules)
+admin.site.register(Tag)
--- learn_cryptography/users/models.py
+++ learn_cryptography/users/models.py
@@ -2,12 +2,14 @@ from turtle import width
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='profile-pics/default.jpg', upload_to="profile-pics")
+    currentLevel = models.CharField(max_length=100, default='Nothing')
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return f'{self.user} Profile'
 
     def save(self):
         super().save()
